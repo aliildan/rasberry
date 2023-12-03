8cc434a8a1c2fe15cfdaae57a0c5dbf57924c36a
Duru <dr.duru.ildan@gmail.com>
2023-12-03 16:17:09 +0100

    LED for loop

--- led.py
+++ led.py
@@ -1,22 +1,14 @@
 from gpiozero import LED
 from time import sleep
 
-beyazLed = LED(17)
-beyazLed.on()
-sleep(2)
-beyazLed.off()
-beyazLed.on()
-sleep(2)
-beyazLed.off()
-beyazLed.on()
-sleep(2)
-beyazLed.off()
-beyazLed.on()
-sleep(2)
-beyazLed.off()
-beyazLed.on()
-sleep(2)
-beyazLed.off()
-beyazLed.on()
-sleep(2)
-beyazLed.off()
+led = LED(17)
+
+for i in range(10):
+    print(i)
+    print("led on")
+    led.on()
+    sleep(2)
+    print("led off")
+    led.off()
+    sleep(1)
+    i = i + 1
