3bf119e2da30c0d3c3bda8df0bfe3a1503abecdb
Duru <dr.duru.ildan@gmail.com>
2023-12-03 17:57:39 +0100

    BUTTON 5

--- button.py
+++ button.py
@@ -1,8 +1,11 @@
 from gpiozero import Button
-btn = Button(2)
-while True:
-    if btn.is_pressed:
-        print("button basili")
-    else:
-        print("button basili degil")
+btn = Button(17)
 
+def button_pressed():
+    print("button a basildi :)")
+
+def button_released():
+    print("buton birakildi :(")
+
+btn.when_activated= button_pressed
+btn.when_deactivated= button_released
