3f22250ede765a9e1cca9be29fec67b3678990a3
Duru <dr.duru.ildan@gmail.com>
2023-12-03 16:34:36 +0100

    LED forever loop

--- led.py
+++ led.py
@@ -3,12 +3,16 @@ from time import sleep
 
 led = LED(17)
 
-for i in range(10):
-    print(i)
-    print("led on")
+# for loop
+# for i in range(10):
+#     print(i)
+#     print("led on")
+#     led.on()
+#     sleep(2)
+#     print("led off")
+#     led.off()
+#     sleep(1)
+#     i = i + 1
+
+while True:
     led.on()
-    sleep(2)
-    print("led off")
-    led.off()
-    sleep(1)
-    i = i + 1
