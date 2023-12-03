4ea843cd03ba551ac48f8ea6cda506d58d4a52d1
Duru <dr.duru.ildan@gmail.com>
2023-12-03 17:42:40 +0100

    BUTTON 4

--- button.py
+++ button.py
@@ -1,5 +1,8 @@
 from gpiozero import Button
 btn = Button(2)
-btn.wait_for_press()
-print("buton a basildi")
+while True:
+    if btn.is_pressed:
+        print("button basili")
+    else:
+        print("button basili degil")
 
