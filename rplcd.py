3c162ddde20aef0de7eed6b234a584a15c2825e9
aliildan <ali.ildan@gmail.com>
2023-12-13 21:21:52 +0100

    rplcd-2

--- rplcd.py
+++ rplcd.py
@@ -1,8 +1,7 @@
-from RPLCD.i2c import CharLCD
+from RPLCD.gpio import CharLCD
 from signal import pause
-LCD = CharLCD('PCF8574', 0x27)
+LCD = CharLCD()
 
 LCD.write_string('Hello World')
-
 
 pause()
