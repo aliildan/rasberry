da110ca1b2e725aa048d9eaf0ea5f81ae557185a
aliildan <ali.ildan@gmail.com>
2023-12-16 01:24:59 +0100

    weather app screen fix -1

--- weather_app/main.py
+++ weather_app/main.py
@@ -10,12 +10,13 @@ lcd = CharLCD('PCF8574', 0x27)
 
 while True:
     weather = api.get_weather()
-    lcd.create_char(1,weather.get_icon())
+    icon_binary = tuple(int(binary, 2) for binary in weather.get_icon())
+    lcd.create_char(1,icon_binary)
     datetime_obj = datetime.fromisoformat(weather.get_time())
     formatted_datetime = datetime_obj.strftime("%d-%m-%Y %H:%M")
-    lcd.cursor_pos(0,0)
+    lcd.cursor_pos(0, 0)
     lcd.write(1)
-    first_row = f'{weather.get_temperature()} C , Wind: {weather.get_wind_speed()}km/h'
+    first_row = f'{weather.get_temperature()}Â°C , {weather.get_wind_speed()}km/h'
     lcd.write_string(first_row)
     lcd.cursor_pos(0, 1)
     lcd.write_string(formatted_datetime)
