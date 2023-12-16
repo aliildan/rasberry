f82ebae70662085ea1456fa04f0a78324ae98dd2
aliildan <ali.ildan@gmail.com>
2023-12-16 01:49:30 +0100

    weather app screen fix -5

--- weather_app/main.py
+++ weather_app/main.py
@@ -14,11 +14,13 @@ while True:
     lcd.create_char(2, (0b00000000, 0b00011000, 0b00111110, 0b01111100, 0b11111000, 0b01111100, 0b00111110, 0b00011000))
     datetime_obj = datetime.fromisoformat(weather.get_time())
     formatted_datetime = datetime_obj.strftime("%d-%m-%Y %H:%M")
+    lcd.cursor_pos = (0, 0)
     lcd.write(1)
     first_row = f' {weather.get_temperature()}C,'
     lcd.write_string(first_row)
     lcd.write(2)
-    first_row_2 = f' {weather.get_wind_speed()}km/h'
+    first_row_2 = f'{weather.get_wind_speed()}km/h'
     lcd.write_string(first_row_2)
+    lcd.cursor_pos = (1, 0)
     lcd.write_string(formatted_datetime)
-    sleep(120)
+    sleep(60)
